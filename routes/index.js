6c9162af4d77e35445f066edb1804578f96e350a
Jeff Steward <steward.jeff@gmail.com>
2019-09-08 19:34:56 -0400

    initial homepage

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Lightbox' });
 });
 
 /* GET screens page. */
 router.get('/screens', function(req, res, next) {
-  res.render('screens', { title: 'Express' });
+  res.render('screens', { title: 'Screens | Lightbox' });
 });
 
 /* GET shades page. */
 router.get('/shades', function(req, res, next) {
-  res.render('shades', { title: 'Express' });
+  res.render('shades', { title: 'Shades | Lightbox' });
 });
 
 /* GET controller page. */
 router.get('/controller', function(req, res, next) {
-  res.render('controller', { title: 'Express', layout: 'layout-controller' });
+  res.render('controller', { title: 'Controller | Lightbox', layout: 'layout-controller' });
 });
 
 module.exports = router;
