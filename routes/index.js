183041ad33ac004f64d3152a1dc5a3789a71bba3
Jeff Steward <steward.jeff@gmail.com>
2022-01-19 12:01:02 -0500

    clean up styling by including Bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../public/data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lightbox' });
+  res.render('index', { title: `${data.project.title} | Lightbox`, project: data.project });
 });
 
 /* GET screens page. */
 router.get('/screens', function(req, res, next) {
-  res.render('screens', { title: 'Screens | Lightbox' });
+  res.render('screens', { title: `Screens | ${data.project.title} | Lightbox`, data: data });
 });
 
 /* GET shades page. */
 router.get('/shades', function(req, res, next) {
-  res.render('shades', { title: 'Shades | Lightbox' });
+  res.render('shades', { title: `Shades | ${data.project.title} | Lightbox`, data: data });
 });
 
 /* GET controller page. */
 router.get('/controller', function(req, res, next) {
-  res.render('controller', { title: 'Controller | Lightbox', layout: 'layout-controller' });
+  res.render('controller', { title: `Controller | ${data.project.title} | Lightbox`, layout: 'layout-controller', data: data });
 });
 
 module.exports = router;
